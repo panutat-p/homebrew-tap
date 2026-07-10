4fb98f81f2755bdaca11b87765748b8ac0144629
Panutat P <panutat.pree@kkpfg.com>
2026-07-10 22:11:46 +0700

    feat: add hyper-zen formula release automation

---
+++ .github/formula-templates/hyper-zen.rb
@@ -0,0 +1,36 @@
+class HyperZen < Formula
+  desc "Native macOS automation toolkit"
+  homepage "https://github.com/panutat-p/hyper-zen"
+  version "__VERSION__"
+  url "https://github.com/panutat-p/hyper-zen/releases/download/v#{version}/hyper-zen-#{version}.tar.gz"
+  sha256 "__SHA256__"
+
+  depends_on macos: :ventura
+
+  def install
+    bin.install "hyper-zen" => "hyper-zen-#{version}"
+  end
+
+  service do
+    run [opt_bin/"hyper-zen-#{version}", "status-icon"]
+  end
+
+  def caveats
+    <<~EOS
+      Hyper Zen requires Accessibility permission for Teams presence.
+
+      After each upgrade, enable the newly installed executable in:
+      System Settings > Privacy & Security > Accessibility
+
+      Add:
+        #{bin}/hyper-zen-#{version}
+
+      Remove the previous Hyper Zen entry, then restart the service:
+        brew services restart hyper-zen
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hyper-zen-#{version} version")
+  end
+end
